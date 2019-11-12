f04886bdd99d6d6c4caf8452e6869569670f667b
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-11-11 23:15:39 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('JS linked.');
+
+const burger  = document.querySelector('.burger'),
+      bLine1  = document.querySelector('.line2'),
+      bLine2  = document.querySelector('.line3'),
+      nav     = document.querySelector('.headNav'),
+      header  = document.querySelector('.mainHeader');
+
+      function openNav(){
+        console.log('Nav toggled.');
+
+        bLine1.classList.toggle('open');
+        bLine2.classList.toggle('open2');
+        nav.classList.toggle('open');
+        header.classList.toggle('color');
+    }
+burger.addEventListener('click', openNav);
