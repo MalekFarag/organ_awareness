296bd279116fb875a0b7338cad4a8be60c108d02
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-11-18 17:02:26 -0500

    most code done: need images + media queries

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const burger  = document.querySelector('.burger'),
       bLine1  = document.querySelector('.line2'),
       bLine2  = document.querySelector('.line3'),
       nav     = document.querySelector('.headNav'),
-      header  = document.querySelector('.mainHeader');
+      header  = document.querySelector('.mainHeader'),
+      body    = document.querySelector('body');
 
       function openNav(){
         console.log('Nav toggled.');
@@ -12,6 +13,7 @@ const burger  = document.querySelector('.burger'),
         bLine1.classList.toggle('open');
         bLine2.classList.toggle('open2');
         nav.classList.toggle('open');
-        header.classList.toggle('color');
+        body.classList.toggle('color');
     }
+
 burger.addEventListener('click', openNav);
