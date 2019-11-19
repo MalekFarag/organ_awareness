72196c3f6664ded983ec8cb8b6a2f23a761553ff
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-11-18 20:15:26 -0500

    site complete

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ const burger  = document.querySelector('.burger'),
       bLine1  = document.querySelector('.line2'),
       bLine2  = document.querySelector('.line3'),
       nav     = document.querySelector('.headNav'),
-      header  = document.querySelector('.mainHeader'),
-      body    = document.querySelector('body');
+      header  = document.querySelector('.mainHeader');
 
       function openNav(){
         console.log('Nav toggled.');
@@ -13,7 +12,6 @@ const burger  = document.querySelector('.burger'),
         bLine1.classList.toggle('open');
         bLine2.classList.toggle('open2');
         nav.classList.toggle('open');
-        body.classList.toggle('color');
     }
 
 burger.addEventListener('click', openNav);
